e26a450bac8edb31bc62f54f4a4557dbdab2f816
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-12-20 14:51:03 +0100

    all

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Thank to jonathan M pour l'aide
 
 //initialise en cherchant le main et btn 
-let slctMain = document.querySelector('main');
+let main = document.querySelector('main');
 let slctBtn = document.getElementById('btn');
 slctBtn.addEventListener('click', addQuote);
 
@@ -13,8 +13,7 @@ try{
     let response = await fetch("https://thatsthespir.it/api");
     let quote = await response.json(); // await plus .json() après response pour préciser que je prend les infos
     let addArticle = document.createElement("article"); // créa° de l'article qui va contenir les quotes
-    addArticle.innerText = quote.quote + " " + quote.author  // add inner text quote.quote récupère l'infi dans l'objet, si juste quote me met en rep sur internet :[objet objet]
-    slctMain.prepend(addArticle);
+    main.prepend(addArticle);
     displayQuote(addArticle, quote);
 }
 
@@ -27,14 +26,24 @@ catch(error) {
 // créa° affichage quote
 function displayQuote(addArticle, quote) { 
 let figure = document.createElement("figure");
+//let img = document.createElement("image");
+//img.setAttribute('src', quote.photo);
 let blockquote = document.createElement("blockquote");
+blockquote.setAttribute("cite", "https://thatsthespir.it/")
 let h2 = document.createElement("h2");
-let p = document.createElement("p");
+let q = document.createElement("q");
+
+q.innerText = quote.quote;
+h2.innerText = quote.author;
+//addArticle.innerHtml = quote.photo
+
+
 
 blockquote.appendChild(h2);
 figure.appendChild(blockquote);
-blockquote.appendChild(p);
+blockquote.appendChild(q);
 addArticle.appendChild(figure);
+
 
 }
 
